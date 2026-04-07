cb24d5e68fbbef58b02769bd8f1707b6dfe67f66
ryu10 <ryu10nt@gmail.com>
2026-04-06 18:07:25 -0700

    fix battery level formula

--- forth/battery.fs
+++ forth/battery.fs
@@ -1,9 +1,11 @@
 \ GPIO 10 is connected to the battery voltage divider, which is used to measure the battery voltage.
 10 constant BATTERY_PIN
 \ the full battery level - it depends on the battery on your CardPuter.
-2775 constant BATTERY_FULL
+2502 constant BATTERY_FULL
+\ the empty battery level - it depends on the battery on your CardPuter.
+1937 constant BATTERY_EMPTY
 
 \ note this is a very rough estimation of the battery level, it may not be accurate and may need calibration for your specific battery and voltage divider.
 : get-battery-level ( -- n )
-BATTERY_PIN analogRead 100 BATTERY_FULL */ 
+BATTERY_PIN analogRead BATTERY_EMPTY - 100 BATTERY_FULL BATTERY_EMPTY - */ 
 ;
